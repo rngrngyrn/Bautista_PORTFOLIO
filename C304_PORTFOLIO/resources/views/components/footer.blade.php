c98a55275e2799593bb6e4f118a5838a6d4d050a
rngrngyrn <mehmereng1993@gmail.com>
2025-11-18 16:50:48 +0800

    login, register, header,footer,yes
    yes

---
+++ C304_PORTFOLIO/resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <div style="width:100%; background-color:brown">
+            <P>Copyright 2025</P>
+        </div>
+    </body>
+    
+</html>
--- C304_PORTFOLIO/routes/web.php
+++ C304_PORTFOLIO/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\mainController;
 
 route::get('/', [mainController::class, 'index']);
+route::get('/register', [AuthController::class, 'showRegister'])->name('register.form');
